7f1211af6de40b67cc616ce8f4363a7453f0a28e
Devon Rizzo <devonnrizzo@gmail.com>
2020-02-18 12:24:38 -0500

    Question 1 complete

--- Program.cs
+++ Program.cs
@@ -6,43 +6,64 @@ namespace Assignment2_Group6
     {
         static void Main(string[] args)
         {
+            //QUESTION 1
             Console.WriteLine("Question 1");
             int[] l1 = new int[] { 5, 6, 6, 9, 9, 12 };
             int target = 9;
             int[] r = TargetRange(l1, target);
-            // Write your code to print range r here
+            Console.WriteLine("[{0}]", string.Join(", ", r));
+            Console.WriteLine();
+
+            //QUESTION 2
             Console.WriteLine("Question 2");
             string s = "University of South Florida";
             string rs = StringReverse(s);
             Console.WriteLine(rs);
+
+            //QUESTION 3
             Console.WriteLine("Question 3");
             int[] l2 = new int[] { 2, 2, 3, 5, 6 };
             int sum = MinimumSum(l2);
             Console.WriteLine(sum);
+
+            //QUESTION 4
             Console.WriteLine("Question 4");
             string s2 = "Dell";
             string sortedString = FreqSort(s2);
             Console.WriteLine(sortedString);
+
+            //QUESTION 5 - PART 1
             Console.WriteLine("Question 5-Part 1");
             int[] nums1 = { 1, 2, 2, 1 };
             int[] nums2 = { 2, 2 };
             int[] intersect1 = Intersect1(nums1, nums2);
+
             Console.WriteLine("Part 1- Intersection of two arrays is: ");
             DisplayArray(intersect1);
             Console.WriteLine("\n");
+
+
+            //QUESTION 5 - PART 2
             Console.WriteLine("Question 5-Part 2");
             int[] intersect2 = Intersect2(nums1, nums2);
+
             Console.WriteLine("Part 2- Intersection of two arrays is: ");
             DisplayArray(intersect2);
             Console.WriteLine("\n");
+
+            //QUESTION 6 
             Console.WriteLine("Question 6");
             char[] arr = new char[] { 'a', 'g', 'h', 'a' };
             int k = 3;
             Console.WriteLine(ContainsDuplicate(arr, k));
+
+            //QUESTION 7 
             Console.WriteLine("Question 7");
             int rodLength = 4;
             int priceProduct = GoldRod(rodLength);
             Console.WriteLine(priceProduct);
+
+            //QUESTION 8
             Console.WriteLine("Question 8");
             string[] userDict = new string[] { "rocky", "usf", "hello", "apple" };
             string keyword = "hhllo";
@@ -50,6 +71,7 @@ namespace Assignment2_Group6
             Console.WriteLine("Question 8");
             SolvePuzzle();
         }
+
         public static void DisplayArray(int[] a)
         {
             foreach (int n in a)
@@ -57,18 +79,37 @@ namespace Assignment2_Group6
                 Console.Write(n + " ");
             }
         }
+
         public static int[] TargetRange(int[] l1, int t)
         {
+            bool found = false;
+            int index = 0;
+            int position = -1;
+            int position2 = -1;
+
             try
             {
-                //Write your code here;
+                //Scan through the array 
+                while (!found && index < l1.Length)
+                {
+                    if (l1[index] == t)
+                    {
+                        found = true;
+                        position = index;
+                        position2 = index + 1;
+                    }
+
+                    index++;
+                }
             }
             catch (Exception)
             {
                 throw;
             }
-            return new int[] { };
+
+           return new int[] {position, position2};
         }
+
         public static string StringReverse(string s)
         {
             try
@@ -81,6 +122,7 @@ namespace Assignment2_Group6
             }
             return null;
         }
+
         public static int MinimumSum(int[] l2)
         {
             try
@@ -93,6 +135,7 @@ namespace Assignment2_Group6
             }
             return 0;
         }
+
         public static string FreqSort(string s2)
         {
             try
@@ -105,6 +148,7 @@ namespace Assignment2_Group6
             }
             return null;
         }
+
         public static int[] Intersect1(int[] nums1, int[] nums2)
         {
             try
@@ -117,6 +161,7 @@ namespace Assignment2_Group6
             }
             return new int[] { };
         }
+
         public static int[] Intersect2(int[] nums1, int[] nums2)
         {
             try
@@ -129,6 +174,7 @@ namespace Assignment2_Group6
             }
             return new int[] { };
         }
+
         public static bool ContainsDuplicate(char[] arr, int k)
         {
             try
@@ -141,6 +187,7 @@ namespace Assignment2_Group6
             }
             return default;
         }
+
         public static int GoldRod(int rodLength)
         {
             try
@@ -153,6 +200,7 @@ namespace Assignment2_Group6
             }
             return 0;
         }
+
         public static bool DictSearch(string[] userDict, string keyword)
         {
             try
@@ -165,6 +213,7 @@ namespace Assignment2_Group6
             }
             return default;
         }
+
         public static void SolvePuzzle()
         {
             try
@@ -175,8 +224,8 @@ namespace Assignment2_Group6
             {
                 throw;
             }
-
         }
+
     }
 }
 
